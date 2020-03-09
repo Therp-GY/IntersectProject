292a79793be058f872a9ded199ae9aad8178b0cb
Therp-GY <466609821@qq.com>
2020-03-09 21:21:23 +0800

    unordered_map

--- src/find_Inter.cpp
+++ src/find_Inter.cpp
@@ -2,19 +2,21 @@
 #include<string>
 #include <vector>
 #include <algorithm>
-#include <map>
+#include <unordered_map>
 #include "my_math.h"
 using namespace std;
 
-void find_intersection_l_l(Line& l, vector<Line>& l_list, map<Point, int>& p_map);
-void find_intersection_l_c(Line& l, vector<Circle>& c_list, map<Point, int>& p_map);
-void find_intersection_c_l(Circle& c, vector<Line>& l_list, map<Point, int>& p_map);
-void find_intersection_c_c(Circle& c, vector<Circle>& c_list, map<Point, int>& p_map);
+void find_intersection_l_l(Line& l, vector<Line>& l_list, unordered_map<Point, int>& p_unordered_map);
+void find_intersection_l_c(Line& l, vector<Circle>& c_list, unordered_map<Point, int>& p_unordered_map);
+void find_intersection_c_l(Circle& c, vector<Line>& l_list, unordered_map<Point, int>& p_unordered_map);
+void find_intersection_c_c(Circle& c, vector<Circle>& c_list, unordered_map<Point, int>& p_unordered_map);
+
+
 
 
 int main() {
-	map<Point, int>point_map;
-	map<Point, int>::iterator point_it;
+	unordered_map<Point, int>point_unordered_map;
+	unordered_map<Point, int>::iterator point_it;
 	vector<Line> line_list;
 	vector<Line>::iterator line_it;
 	vector<Circle>circle_list;
@@ -37,8 +39,8 @@ int main() {
 			cin >> y2;
 			Point p2(x2, y2);
 			Line l(p1, p2);
-			find_intersection_l_l(l, line_list, point_map);
-			find_intersection_l_c(l, circle_list, point_map);
+			find_intersection_l_l(l, line_list, point_unordered_map);
+			find_intersection_l_c(l, circle_list, point_unordered_map);
 			line_list.push_back(l);
 
 		}
@@ -48,8 +50,8 @@ int main() {
 			Point p1(x1, y1);
 			cin >> r;
 			Circle circle(p1, r);
-			find_intersection_c_l(circle, line_list, point_map);
-			find_intersection_c_c(circle, circle_list, point_map);
+			find_intersection_c_l(circle, line_list, point_unordered_map);
+			find_intersection_c_c(circle, circle_list, point_unordered_map);
 			circle_list.push_back(circle);
 
 		}
@@ -59,68 +61,68 @@ int main() {
 			i--;
 		}
 	}
-	cout << point_map.size() << endl;
-	//for (point_it = point_map.begin(); point_it != point_map.end(); point_it++) {	
+	cout << point_unordered_map.size() << endl;
+	//for (point_it = point_unordered_map.begin(); point_it != point_unordered_map.end(); point_it++) {	
 	//	cout << point_it ->first.get_x() << "," << point_it->first.get_y() << endl;
 	//}
 
 }
 
-void find_intersection_l_l(Line &l,vector<Line> &l_list, map<Point, int>&p_map) {
+void find_intersection_l_l(Line &l,vector<Line> &l_list, unordered_map<Point, int>&p_unordered_map) {
 	for (unsigned int i = 0; i < l_list.size(); i++) {
 		Point p;
 		Point nopoint(INFINITY, INFINITY);
 		p = l_list[i].find_intersection(l);
 		if (p == nopoint)continue;
-		p_map.insert(pair<Point, int>(p, 1));
+		p_unordered_map.insert(pair<Point, int>(p, 1));
 	}
 }
 
-void find_intersection_l_c(Line &l, vector<Circle>& c_list, map<Point, int>& p_map) {
+void find_intersection_l_c(Line &l, vector<Circle>& c_list, unordered_map<Point, int>& p_unordered_map) {
 	for (unsigned int i = 0; i < c_list.size(); i++) {
 		Point p[2];
 		int n;
 		n = c_list[i].find_intersection(l, p);
 		if (n == 0)continue;
 		else if (n == 1) {
-			p_map.insert(pair<Point, int>(p[0], 1));
+			p_unordered_map.insert(pair<Point, int>(p[0], 1));
 		}
 		else {
-			p_map.insert(pair<Point, int>(p[0], 1));
-			p_map.insert(pair<Point, int>(p[1], 1));
+			p_unordered_map.insert(pair<Point, int>(p[0], 1));
+			p_unordered_map.insert(pair<Point, int>(p[1], 1));
 		}
 	}
 }
 
-void find_intersection_c_l(Circle &c, vector<Line>& l_list, map<Point, int>& p_map) {
+void find_intersection_c_l(Circle &c, vector<Line>& l_list, unordered_map<Point, int>& p_unordered_map) {
 	for (unsigned int i = 0; i < l_list.size(); i++) {
 		Point p[2];
 		int n;
 		n = c.find_intersection(l_list[i], p);
 			if (n == 0)continue;
 		else if (n == 1) {
-			p_map.insert(pair<Point, int>(p[0], 1));
+			p_unordered_map.insert(pair<Point, int>(p[0], 1));
 		}
 		else {
-			p_map.insert(pair<Point, int>(p[0], 1));
-			p_map.insert(pair<Point, int>(p[1], 1));	
+			p_unordered_map.insert(pair<Point, int>(p[0], 1));
+			p_unordered_map.insert(pair<Point, int>(p[1], 1));	
 		}
 	}
 }
 
 
-void find_intersection_c_c(Circle &c, vector<Circle>& c_list, map<Point, int>& p_map) {
+void find_intersection_c_c(Circle &c, vector<Circle>& c_list, unordered_map<Point, int>& p_unordered_map) {
 	for (unsigned int i = 0; i < c_list.size(); i++) {
 		Point p[2];
 		int n;
 		n = c_list[i].find_intersection(c, p);
 		if (n == 0)continue;
 		else if (n == 1) {
-			p_map.insert(pair<Point, int>(p[0], 1));	
+			p_unordered_map.insert(pair<Point, int>(p[0], 1));	
 		}
 		else {
-			p_map.insert(pair<Point, int>(p[0], 1));
-			p_map.insert(pair<Point, int>(p[1], 1));
+			p_unordered_map.insert(pair<Point, int>(p[0], 1));
+			p_unordered_map.insert(pair<Point, int>(p[1], 1));
 		}
 	}
 }
--- src/my_math.h
+++ src/my_math.h
@@ -2,6 +2,34 @@
 #define MATH_H
 #include <ostream>
 #include <iostream>
+#include <functional>	
+
+template<typename T>
+inline void hash_combine(std::size_t& seed, const T& val)
+{
+	seed ^= std::hash<T>()(val) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+template<typename T>
+inline void hash_val(std::size_t& seed, const T& val)
+{
+	hash_combine(seed, val);
+}
+
+template<typename T, typename... Types>
+inline void hash_val(std::size_t& seed, const T& val, const Types& ... args)
+{
+	hash_combine(seed, val);
+	hash_val(seed, args...);
+}
+
+template<typename... Types>
+inline std::size_t hash_val(const Types& ...args)
+{
+	std::size_t seed = 0;
+	hash_val(seed, args...);
+	return seed;
+}
 
 class Point {
 	double x_;
@@ -20,6 +48,18 @@ public:
 	Point operator/(const double& d) const;
 	friend void operator<<(std::ostream &os,  Point &point);
 };
+
+namespace std
+{
+	template<>
+	struct hash<Point>
+	{
+		size_t operator() (const Point& s) const noexcept
+		{
+			return hash_val(s.get_x(), s.get_y());
+		}
+	}; // 间接调用原生Hash.
+}
 
 typedef Point Vector;
 
@@ -55,4 +95,7 @@ public:
 };
 
 bool equal(const double& a, const double& b);
+
+
+
 #endif
