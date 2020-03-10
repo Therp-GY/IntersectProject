dafda495f77755af91c9dd42a5ad3a1fa846f9da
Therp-GY <466609821@qq.com>
2020-03-10 11:17:45 +0800

    add -i -o

--- src/find_Inter.cpp
+++ src/find_Inter.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include<fstream>
 #include "my_math.h"
 using namespace std;
 
@@ -14,28 +15,38 @@ void find_intersection_c_c(Circle& c, vector<Circle>& c_list, set<Point>& p_set)
 
 
 
-int main() {
+int main(int argc, char *argv[]) {
+	ofstream fout;
+	ifstream fin;
 	set<Point>point_set;
 	vector<Line> line_list;
 	vector<Line>::iterator line_it;
 	vector<Circle>circle_list;
 	vector<Circle>::iterator circle_it;
 
+	for(int i = 1; i < argc; i++) {
+		if ((string)argv[i] == "-i") {
+			fin.open(argv[i + 1]);
+		}
+		if ((string)argv[i] == "-o") {
+			fout.open(argv[i + 1]);
+		}
+	}
+
 	//	去重方法
 	/*line_list.erase(unique(line_list.begin(), line_list.end()), line_list.end);*/
-	cout << "how many inputs ?" << endl;
 	int n;
-	cin >> n;
+	fin >> n;
 	for (int i = 0; i < n; i++) {
 		char type;
 		double x1, y1, x2, y2, r;
-		cin >> type;
+		fin >> type;
 		if (type == 'L') {
-			cin >> x1;
-			cin >> y1;
+			fin >> x1;
+			fin >> y1;
 			Point p1(x1, y1);
-			cin >> x2;
-			cin >> y2;
+			fin >> x2;
+			fin >> y2;
 			Point p2(x2, y2);
 			Line l(p1, p2);
 			find_intersection_l_l(l, line_list, point_set);
@@ -44,10 +55,10 @@ int main() {
 
 		}
 		else if(type == 'C'){
-			cin >> x1;
-			cin >> y1;
+			fin >> x1;
+			fin >> y1;
 			Point p1(x1, y1);
-			cin >> r;
+			fin >> r;
 			Circle circle(p1, r);
 			find_intersection_c_l(circle, line_list, point_set);
 			find_intersection_c_c(circle, circle_list, point_set);
@@ -55,14 +66,14 @@ int main() {
 
 		}
 		else {
-			cout << "wrong input" << endl;
-			cin.ignore(100, '\n');
+			fout << "wrong input" << endl;
+			fin.ignore(100, '\n');
 			i--;
 		}
 	}
-	cout << point_set.size() << endl;
+	fout << point_set.size() << endl;
 	//for (point_it = point_set.begin(); point_it != point_set.end(); point_it++) {	
-	//	cout << point_it ->first.get_x() << "," << point_it->first.get_y() << endl;
+	//	fout << point_it ->first.get_x() << "," << point_it->first.get_y() << endl;
 	//}
 
 }
